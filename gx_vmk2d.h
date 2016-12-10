327eb6f9ea9e8796e5843886305f463026cf2039
brummer <brummer-@web.de>
2016-12-10 07:55:56 +0100

    * add GUI

--- gx_vmk2d.h
+++ gx_vmk2d.h
@@ -25,7 +25,7 @@
 #include <lv2.h>
 
 #define GXPLUGIN_URI "http://guitarix.sourceforge.net/plugins/gx_vmk2d_"
-#define GXPLUGIN_UI_URI "http://guitarix.sourceforge.net/plugins/gx_vmk2d_gui"
+#define GXPLUGIN_UI_URI "http://guitarix.sourceforge.net/plugins/gx_vmk2d_gui#_vmk2d_"
 
 
 typedef enum
